b2f3c2908ae1be19cb860756b91b1d659bb7f3f2
Michael Drogalis <mjd3089@rit.edu>
2015-04-23 21:09:32 -0700

    Bring in r-seq.

--- src/onyx_metrics/core.clj
+++ src/onyx_metrics/core.clj
@@ -1,6 +1,7 @@
-(ns onyx-metrics.core)
+(ns onyx-metrics.core
+  (:require [rotating-seq.core :as rsc]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def r (rsc/create-r-seq 60000 20000))
+
+(rsc/expire-bucket (rsc/expire-bucket (rsc/expire-bucket (rsc/add-to-head (rsc/add-to-head r [1 2 3]) [4 5]))))
+
