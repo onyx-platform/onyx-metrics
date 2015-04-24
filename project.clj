6d2e7e2950cfcf4b3a6e08c1fc814d147df63217
Michael Drogalis <mjd3089@rit.edu>
2015-04-23 21:13:38 -0700

    Bring in Incanter.

--- project.clj
+++ project.clj
@@ -4,4 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [com.mdrogalis/rotating-seq "0.1.1"]])
+                 [com.mdrogalis/rotating-seq "0.1.1"]
+                 [incanter/incanter-core "1.9.0"]])
