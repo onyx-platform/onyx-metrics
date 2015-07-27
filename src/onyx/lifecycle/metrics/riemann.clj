b9389f19b0fa4a836d2f7993dedf23741f134184
Admin <admin@Gardner-Vickers-Mac.local>
2015-07-27 10:50:38 -0400

    Correctly outputs latency and throughput statistics to riemann

--- src/onyx/lifecycle/metrics/riemann.clj
+++ src/onyx/lifecycle/metrics/riemann.clj
@@ -13,38 +13,33 @@
        (loop []
          (Thread/sleep (:riemann/interval-ms lifecycle))
          (let [state @(:onyx.metrics/state event)
-               client (r/tcp-client {:host (:riemann/client event)})
-               name   (:riemann/name event)
+               client (r/tcp-client {:host (:riemann/client lifecycle)})
+               name   (:riemann/name lifecycle)
                task-name (str (:onyx.core/task event))]
-           (println (str "HELLO, I GOT HERE\n"
-                         "state: " state
-                         "\nclient: " client
-                         "\nname: " name
-                         "\ntask-name: " task-name))
            (when-let [throughput (:throughput state)]
-             (r/send-event client {:service (format "[%s][%s] 1s_throughput" name task-name)
+             (r/send-event client {:service (format "[%s] 1s_throughput" task-name)
                                    :state "ok" :metric (apply + (map #(apply + %) (take 1 throughput)))
-                                   :tags ["throughput" "onyx" task-name name]})
+                                   :tags ["throughput_1s" "onyx" task-name name]})
 
-             (r/send-event client {:service (format "[%s][%s] 10s_throughput" name task-name)
+             (r/send-event client {:service (format "[%s] 10s_throughput" task-name)
                                    :state "ok" :metric (apply + (map #(apply + %) (take 10 throughput)))
                                    :tags ["throughput_10s" "onyx" task-name name]})
 
-             (r/send-event client {:service (format "[%s][%s] 60s_throughput" name task-name)
+             (r/send-event client {:service (format "[%s] 60s_throughput" task-name)
                                    :state "ok" :metric (apply + (map #(apply + %) (take 60 throughput)))
                                    :tags ["throughput_60s" "onyx" task-name name]}))
            (when-let [latency (:latency state)]
-             (r/send-event client {:service (format "[%s][%s] 10s_latency_50_percentile %s" name task-name)
+             (r/send-event client {:service (format "[%s] 50_percentile_latency" task-name)
                                    :state "ok" :metric (quantile 0.50 (apply concat (take 10 latency)))
-                                   :tags ["latency" "onyx" "50_percentile" task-name name]})
+                                   :tags ["latency_50th" "onyx" "50_percentile" task-name name]})
 
-             (r/send-event client {:service (format "[%s][%s] 10s_latency_90_percentile %s" name task-name)
+             (r/send-event client {:service (format "[%s] 90_percentile_latency" task-name)
                                    :state "ok" :metric (quantile 0.90 (apply concat (take 10 latency)))
-                                   :tags ["latency" "onyx" task-name name]})
+                                   :tags ["latency_90th" "onyx" task-name name]})
 
-             (r/send-event client {:service (format "[%s][%s] 10s_latency_99_percentile %s" name task-name)
+             (r/send-event client {:service (format "[%s] 99_percentile_latency" task-name)
                                    :state "ok" :metric (quantile 0.99 (apply concat (take 10 latency)))
-                                   :tags ["latency" "onyx" task-name name]}))
+                                   :tags ["latency_99th" "onyx" task-name name]}))
            (recur)))
        (catch InterruptedException e)
        (catch Throwable e
