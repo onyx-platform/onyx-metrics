cfa16237a2591043bca96a815ce47c37e84c5e95
Michael Drogalis <mjd3089@rit.edu>
2015-07-31 19:26:52 -0700

    0.7 upgrade.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.onyxplatform/onyx-metrics "0.6.0"
+(defproject org.onyxplatform/onyx-metrics "0.7.0-SNAPSHOT"
   :description "Instrument Onyx workflows"
   :url "https://github.com/MichaelDrogalis/onyx"
   :license {:name "Eclipse Public License"
@@ -7,6 +7,6 @@
                  [com.mdrogalis/rotating-seq "0.1.3"]
                  [com.taoensso/timbre "3.0.1"]
                  [stylefruits/gniazdo "0.4.0"]]
-  :profiles {:dev {:dependencies [[org.onyxplatform/onyx "0.6.0"]
+  :profiles {:dev {:dependencies [[org.onyxplatform/onyx "0.7.0-SNAPSHOT"]
                                   [midje "1.6.3"]]
                    :plugins [[lein-midje "3.1.1"]]}})
