774e9d8d4adb290eb1af942c20cc7efecc953a45
gardnervickers <gardner@vickers.me>
2015-08-03 17:09:03 -0400

    Added a tage for `:onyx.core/id`. Also, service descriptors are now namespaced by the peer each task is located on

--- src/onyx/lifecycle/metrics/riemann.clj
+++ src/onyx/lifecycle/metrics/riemann.clj
@@ -15,31 +15,35 @@
          (let [state @(:onyx.metrics/state event)
                client (r/tcp-client {:host (:riemann/address lifecycle)})
                name   (:riemann/workflow-name lifecycle)
-               task-name (str (:onyx.core/task event))]
+               task-name (str (:onyx.core/task event))
+               peer-id (str (:onyx.core/id event))]
            (when-let [throughput (:throughput state)]
-             (r/send-event client {:service (format "[%s] 1s_throughput" task-name)
+             (r/send-event client {:service (format "[%s|%s] 1s_throughput" peer-id task-name)
                                    :state "ok" :metric (apply + (map #(apply + %) (take 1 throughput)))
-                                   :tags ["throughput_1s" "onyx" task-name name]})
+                                   :tags ["throughput_1s" "onyx" task-name name peer-id]})
 
-             (r/send-event client {:service (format "[%s] 10s_throughput" task-name)
+             (r/send-event client {:service (format "[%s|%s] 10s_throughput" peer-id task-name)
                                    :state "ok" :metric (apply + (map #(apply + %) (take 10 throughput)))
-                                   :tags ["throughput_10s" "onyx" task-name name]})
+                                   :tags ["throughput_10s" "onyx" task-name name peer-id]})
 
-             (r/send-event client {:service (format "[%s] 60s_throughput" task-name)
+             (r/send-event client {:service (format "[%s|%s] 60s_throughput" peer-id task-name)
                                    :state "ok" :metric (apply + (map #(apply + %) (take 60 throughput)))
-                                   :tags ["throughput_60s" "onyx" task-name name]}))
+                                   :tags ["throughput_60s" "onyx" task-name name peer-id]}))
            (when-let [latency (:latency state)]
-             (r/send-event client {:service (format "[%s] 50_percentile_latency" task-name)
+             (r/send-event client {:service (format "[%s|%s] 50_percentile_latency" peer-id task-name)
                                    :state "ok" :metric (quantile 0.50 (apply concat (take 10 latency)))
-                                   :tags ["latency_50th" "onyx" "50_percentile" task-name name]})
+                                   :tags ["latency_50th" "onyx" "50_percentile" task-name name
+                                          peer-id]})
 
-             (r/send-event client {:service (format "[%s] 90_percentile_latency" task-name)
+             (r/send-event client {:service (format "[%s|%s] 90_percentile_latency" peer-id task-name)
                                    :state "ok" :metric (quantile 0.90 (apply concat (take 10 latency)))
-                                   :tags ["latency_90th" "onyx" task-name name]})
+                                   :tags ["latency_90th" "onyx" task-name name
+                                          peer-id]})
 
-             (r/send-event client {:service (format "[%s] 99_percentile_latency" task-name)
+             (r/send-event client {:service (format "[%s|%s] 99_percentile_latency" peer-id task-name)
                                    :state "ok" :metric (quantile 0.99 (apply concat (take 10 latency)))
-                                   :tags ["latency_99th" "onyx" task-name name]}))
+                                   :tags ["latency_99th" "onyx" task-name name
+                                          peer-id]}))
            (recur)))
        (catch InterruptedException e)
        (catch Throwable e
