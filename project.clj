f765a1a12343dc75d63668fde348ee97f2c40f89
Michael Drogalis <mjd3089@rit.edu>
2015-10-22 09:40:03 -0700

    Fix quoting.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.onyxplatform/onyx-metrics "0.7.12-SNAPSHOT"
+(defproject org.onyxplatform/onyx-metrics "0.7.13-SNAPSHOT"
   :description "Instrument Onyx workflows"
   :url "https://github.com/MichaelDrogalis/onyx"
   :license {:name "Eclipse Public License"
