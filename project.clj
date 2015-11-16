0d8a28adcc111b1293dc7d11cec9de27997a2921
Lucas Bradstreet <lucasbradstreet@gmail.com>
2015-11-16 16:59:29 +0800

    0.7.10.1 is a more appropriate release number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.onyxplatform/onyx-metrics "0.7.11"
+(defproject org.onyxplatform/onyx-metrics "0.7.10.1"
   :description "Instrument Onyx workflows"
   :url "https://github.com/MichaelDrogalis/onyx"
   :license {:name "Eclipse Public License"
