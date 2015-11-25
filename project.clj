1931646010dceb0a39cd0eea5ceb4185677c8b53
Lucas Bradstreet <lucasbradstreet@gmail.com>
2015-11-26 04:03:45 +0800

    Move onyx dep out of dev depedencies

--- project.clj
+++ project.clj
@@ -11,16 +11,16 @@
                              :username :env
                              :password :env
                              :sign-releases false}}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [^{:voom {:repo "git@github.com:onyx-platform/onyx.git" :branch "master"}}
+                 [org.onyxplatform/onyx "0.8.1-alpha6"]
+                 [org.clojure/clojure "1.7.0"]
                  [interval-metrics "1.0.0"]
                  [stylefruits/gniazdo "0.4.0"]]
   :java-opts ^:replace ["-server" "-Xmx3g"]
   :global-vars  {*warn-on-reflection* true 
                  *assert* false
                  *unchecked-math* :warn-on-boxed}
-  :profiles {:dev {:dependencies [^{:voom {:repo "git@github.com:onyx-platform/onyx.git" :branch "master"}}
-                                  [org.onyxplatform/onyx "0.8.1-alpha8"]
-                                  [riemann-clojure-client "0.4.1"]]
+  :profiles {:dev {:dependencies [[riemann-clojure-client "0.4.1"]]
                    :plugins [[lein-set-version "0.4.1"]
                              [lein-update-dependency "0.1.2"]
                              [lein-pprint "1.1.1"]]}})
