701693f1dbe3e36d2f2a8fa99bdbd7a56b86ac86
OnyxBot <michael.drogalis@onyxplatform.org>
2015-11-25 19:16:10 +0000

    Release version 0.8.1.0-alpha8.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.onyxplatform/onyx-metrics "0.8.1.0-alpha7"
+(defproject org.onyxplatform/onyx-metrics "0.8.1.0-alpha8"
   :description "Instrument Onyx workflows"
   :url "https://github.com/MichaelDrogalis/onyx"
   :license {:name "Eclipse Public License"
@@ -19,7 +19,7 @@
                  *assert* false
                  *unchecked-math* :warn-on-boxed}
   :profiles {:dev {:dependencies [^{:voom {:repo "git@github.com:onyx-platform/onyx.git" :branch "master"}}
-                                  [org.onyxplatform/onyx "0.8.1-alpha7"]
+                                  [org.onyxplatform/onyx "0.8.1-alpha8"]
                                   [riemann-clojure-client "0.4.1"]]
                    :plugins [[lein-set-version "0.4.1"]
                              [lein-update-dependency "0.1.2"]
