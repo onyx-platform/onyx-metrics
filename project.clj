359b242961ea2538ab1ce86eacb39a847c2682f3
Lucas Bradstreet <lucasbradstreet@gmail.com>
2015-11-30 23:43:12 +0800

    Prepare for next release cycle.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.onyxplatform/onyx-metrics "0.8.2.3"
+(defproject org.onyxplatform/onyx-metrics "0.8.2.4-SNAPSHOT"
   :description "Instrument Onyx workflows"
   :url "https://github.com/MichaelDrogalis/onyx"
   :license {:name "Eclipse Public License"
