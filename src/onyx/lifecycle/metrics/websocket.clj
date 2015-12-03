9c0052f78df934488c4d0781fed2d3c5dc61d322
Lucas Bradstreet <lucasbradstreet@gmail.com>
2015-12-03 15:17:48 +0800

    Fix sender signatures

--- src/onyx/lifecycle/metrics/websocket.clj
+++ src/onyx/lifecycle/metrics/websocket.clj
@@ -3,7 +3,7 @@
             [clojure.core.async :refer [chan >!! <!! close! sliding-buffer]]
             [gniazdo.core :as ws]))
 
-(defn websocket-sender [lifecycle ch _]
+(defn websocket-sender [lifecycle ch]
   (future
     (let [conn (ws/connect (:websocket/address lifecycle))] 
       (loop []
