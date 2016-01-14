d81b558e72e27451d1ed4d01992c290f43d9de66
OnyxBot <michael.drogalis@onyxplatform.org>
2016-01-14 02:47:01 +0000

    Update dependency org.onyxplatform/onyx to version 0.8.4-20160114_023525-g2fc3ffc.
    Automatic commit by lein-unison.

--- project.clj
+++ project.clj
@@ -12,7 +12,7 @@
                              :password :env
                              :sign-releases false}}
   :dependencies [^{:voom {:repo "git@github.com:onyx-platform/onyx.git" :branch "master"}}
-                 [org.onyxplatform/onyx "0.8.4-20160112_014048-g9e003f9"]
+                 [org.onyxplatform/onyx "0.8.4-20160114_023525-g2fc3ffc"]
                  [org.clojure/clojure "1.7.0"]
                  [interval-metrics "1.0.0"]
                  [stylefruits/gniazdo "0.4.0"]]
