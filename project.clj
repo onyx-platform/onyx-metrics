9115475af77078c8378513bb7716abbaf1563b89
OnyxBot <michael.drogalis@onyxplatform.org>
2016-01-29 12:31:39 +0000

    Prepare for next release cycle.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.onyxplatform/onyx-metrics "0.8.7.0-alpha2"
+(defproject org.onyxplatform/onyx-metrics "0.8.7.0-SNAPSHOT"
   :description "Instrument Onyx workflows"
   :url "https://github.com/MichaelDrogalis/onyx"
   :license {:name "Eclipse Public License"
