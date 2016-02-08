8a210b6ccf2b04c408714b0d7b6a46a064366a55
Lucas Bradstreet <lucasbradstreet@gmail.com>
2016-02-09 04:10:58 +0800

    Release version 0.8.8.1.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.onyxplatform/onyx-metrics "0.8.8.1-SNAPSHOT"
+(defproject org.onyxplatform/onyx-metrics "0.8.8.1"
   :description "Instrument Onyx workflows"
   :url "https://github.com/MichaelDrogalis/onyx"
   :license {:name "Eclipse Public License"
