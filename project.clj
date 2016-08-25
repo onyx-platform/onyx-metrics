ee6b1713ff066f0d7db9b34b7693621624481a24
OnyxBot <michael.drogalis@onyxplatform.org>
2016-08-25 20:25:00 +0000

    Release version 0.9.10.0-beta1.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.onyxplatform/onyx-metrics "0.9.9.1-SNAPSHOT"
+(defproject org.onyxplatform/onyx-metrics "0.9.10.0-beta1"
   :description "Instrument Onyx workflows"
   :url "https://github.com/onyx-platform/onyx-metrics"
   :license {:name "Eclipse Public License"
@@ -11,7 +11,7 @@
                              :username :env
                              :password :env
                              :sign-releases false}}
-  :dependencies [[org.onyxplatform/onyx "0.9.9"]
+  :dependencies [[org.onyxplatform/onyx "0.9.10-beta1"]
                  ^{:voom {:repo "git@github.com:onyx-platform/onyx.git" :branch "master"}}
                  [org.clojure/clojure "1.7.0"]
                  [interval-metrics "1.0.0"]]
